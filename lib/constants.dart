2258ed21370a56566c036e719e60974901ffdbbc
StratosGiak <efstratios.giak@gmail.com>
2024-11-17 01:47:45 +0200

    set permanent ip

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,6 +1,6 @@
 import 'package:intl/intl.dart';
 
-const apiUrl = "http://77.49.73.185/api";
+const apiUrl = "http://188.245.190.233/api";
 final dateFormat = DateFormat("dd/MM/yyyy");
 final dateTimeFormat = DateFormat("dd/MM/yyyy | hh:mm");
 final dateTimeFormatDB = DateFormat("yyyy-MM-dd hh:mm:ss");
