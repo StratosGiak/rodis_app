57574a09ea1c80c1320f2025e6ae1edeb8834c9b
StratosGiak <efstratios.giak@gmail.com>
2024-11-20 03:21:21 +0200

    refactor get records/suggestions methods

--- lib/models/suggestions.dart
+++ lib/models/suggestions.dart
@@ -10,4 +10,26 @@ class Suggestions extends ChangeNotifier {
   Map<int, String> products;
   Map<int, String> manufacturers;
   Map<int, String> statuses;
+
+  Suggestions.fromJSON(Map<String, dynamic> map)
+      : products = map['products'],
+        manufacturers = map['manufacturers'],
+        statuses = map['statuses'];
+
+  void setAll(Map<String, dynamic> map) {
+    bool dirty = false;
+    if (products != map['products']) {
+      products = map['products'];
+      dirty = true;
+    }
+    if (manufacturers != map['manufacturers']) {
+      manufacturers = map['manufacturers'];
+      dirty = true;
+    }
+    if (statuses != map['statuses']) {
+      statuses = map['statuses'];
+      dirty = true;
+    }
+    if (dirty) notifyListeners();
+  }
 }
