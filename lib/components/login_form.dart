bcedd97aae42b523986c92bbfdf981f04b0ec7ec
StratosGiak <efstratios.giak@gmail.com>
2024-11-25 02:17:48 +0200

    set username based on login textfield

--- lib/components/login_form.dart
+++ lib/components/login_form.dart
@@ -56,7 +56,7 @@ class _LoginFormState extends State<LoginForm> {
                 Provider(
                   create: (_) => User(
                     id: user['id'],
-                    username: user['username'],
+                    username: usernameController.text,
                     name: user["name"],
                   ),
                 ),
